5659393e91b198ad0adaf2b10c87571325e57ddb
areebshahzad007 <areebshahzad19@hotmail.com>
2023-11-24 00:51:06 +0000

    final version

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-// Import the modules we need..
+// Import the required modules
 var mysql = require('mysql');
-var express = require ('express')
-var ejs = require('ejs')
-var bodyParser= require ('body-parser')
+var express = require('express');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
 
 // Define the database connection
-const db = mysql.createConnection ({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'appuser',
     password: 'app2027',
     database: 'myBookshop'
 });
+
 // Connect to the database
 db.connect((err) => {
     if (err) {
         throw err;
     }
-    console.log('Connected to database');
+    console.log('Connected to the database');
 });
+
+// Make the database connection available globally
 global.db = db;
 
+// Create the Express application object
+const app = express();
+const port = 8000;
 
-// Create the express application object
-const app = express()
-const port = 8000
-app.use(bodyParser.urlencoded({ extended: true }))
+// Use the body-parser middleware for parsing incoming POST request data
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set up css
+// Set up static files (CSS) to be served from the 'public' directory
 app.use(express.static(__dirname + '/public'));
 
-// Set the directory where Express will pick up HTML files
-// __dirname will get the current directory
+// Set the directory where Express will look for views (HTML files)
 app.set('views', __dirname + '/views');
 
-// Tell Express that we want to use EJS as the templating engine
+// Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
-// Tells Express how we should process html files
-// We want to use EJS's rendering engine
+// Configure EJS to process HTML files using its rendering engine
 app.engine('html', ejs.renderFile);
 
-// Define our data
-var shopData = {shopName: "Bertie's Books"}
+// Define the data for the shop
+var shopData = { shopName: "Bertie's Books" };
 
-// Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
-require("./routes/main")(app, shopData);
+// Require the main.js file inside the routes folder, passing in the Express app and shopData as arguments.
+// All the routes will be defined in this file.
+require('./routes/main')(app, shopData);
 
-// Start the web app listening
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Start the web app listening on the specified port
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/main.js
+++ routes/main.js
@@ -1,35 +1,93 @@
+// Exporting a function that sets up routes for the provided Express app and shop data
 module.exports = function(app, shopData) {
 
-    // Handle our routes
+    // Route to display a list of all books
     app.get('/list', function(req, res) {
-        let sqlquery = "SELECT * FROM books"; // query database to get all the books
-        // execute sql query
+        let sqlquery = "SELECT * FROM books"; // SQL query to retrieve all books from the database
+        // Execute SQL query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); // Redirect to the home page if there is an error
+            }
+            let newData = Object.assign({}, shopData, {availableBooks:result});
+            console.log(newData);
+            res.render("list.ejs", newData); // Render the 'list.ejs' template with the retrieved data
+         });
+    });
+
+    // Route to display the home page
+    app.get('/',function(req,res){
+        res.render('index.ejs', shopData);
+    });
+
+    // Route to display the about page
+    app.get('/about',function(req,res){
+        res.render('about.ejs', shopData);
+    });
+
+    // Route to display the search page
+    app.get('/search',function(req,res){
+        res.render("search.ejs", shopData);
+    });
+
+    // Route to display the registration page
+    app.get('/register', function (req,res) {
+        res.render('register.ejs', shopData);
+    });
+
+    // Route to handle registration form submission
+    app.post('/registered', function (req,res) {
+        // Saving data in the database (Note: Database interaction code is assumed to be elsewhere)
+        res.send('Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);
+    });
+
+    // Route to display the add book page
+    app.get('/addbook', function (req,res) {
+        res.render('addbook.ejs', shopData);
+    });
+
+    // Route to handle adding a new book to the database
+    app.post('/bookadded', function (req,res) {
+        // Saving data in the database
+        let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
+        // Execute SQL query
+        let newrecord = [req.body.name, req.body.price];
+        db.query(sqlquery, newrecord, (err, result) => {
+          if (err) {
+            return console.error(err.message);
+          } else {
+            res.send('This book is added to database, name: '
+                      + req.body.name + ' price '+ req.body.price);
+          }
+        });
+    });
+
+    // Route to display a list of bargain books
+    app.get('/bargainbooks', function(req, res) {
+        let sqlquery = "SELECT * FROM books WHERE price < 20"; // SQL query to retrieve bargain books
+        // Execute SQL query
         db.query(sqlquery, (err, result) => {
             if (err) {
                 res.redirect('./'); 
             }
-            res.send(result)
+            res.render('bargainbooks.ejs', { books: result }); // Render the 'bargainbooks.ejs' template and pass the query result as data
          });
     });
 
-    app.get('/',function(req,res){
-        res.render('index.ejs', shopData)
-    });
-    app.get('/about',function(req,res){
-        res.render('about.ejs', shopData);
-    });
-    app.get('/search',function(req,res){
-        res.render("search.ejs", shopData);
-    });
+    // Route to display search results based on a keyword
     app.get('/search-result', function (req, res) {
-        //searching in the database
-        res.send("You searched for: " + req.query.keyword);
+        const keyword = req.query.keyword; // Assuming the search keyword is passed as a query parameter
+
+        // Use a single SQL query with a conditional WHERE clause for exact and partial matches
+        let sqlquery = "SELECT * FROM books WHERE name = ? OR name LIKE ?";
+
+        // Execute SQL query with both exact and partial match conditions
+        db.query(sqlquery, [keyword, '%' + keyword + '%'], (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+            res.render('search-result.ejs', { books: result, keyword: keyword });
+        });
     });
-    app.get('/register', function (req,res) {
-        res.render('register.ejs', shopData);                                                                     
-    });                                                                                                 
-    app.post('/registered', function (req,res) {
-        // saving data in database
-        res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
-    }); 
+
 }
